057f65d26d55f7649c45ff9462b8392d19981b4b
magnusua <uscmagnus@gmail.com>
2021-10-09 19:42:37 +0300

    Collatz conjecture

--- src/scraps/scraps.kt
+++ src/scraps/scraps.kt
@@ -1,8 +1,13 @@
 package scraps
+
+
 import java.util.Collections
+import kotlin.math.abs
+
 class scraps {
 
 }
+
 fun main2() {
     var inputArray: Array<Array<String>> = arrayOf()
     val n = readLine()!!.toInt()
@@ -28,8 +33,6 @@ fun main3() = when (readLine()!!.toInt()) {
 }.let { println(it) }
 
 
-
-
 //Write a program that reads an A list of integers and cyclically shifts the elements of the list to the right:
 //1 number = length of array numbers
 fun main4() = println(List(readLine()!!.toInt()) { readLine()!!.toInt() }
@@ -40,8 +43,58 @@ fun <T> List<T>.rotate(distance: Int) =
     toList().also {
         Collections.rotate(it, distance)
     }
+
 //Write a program that reads a list of integers and outputs the number of triples in the list.
 //1 number = length of array numbers
 fun main5() = println(List(readLine()!!.toInt()) { readLine()!!.toInt() }.run {
     (0..this.size - 3).count { this[it] + 1 == this[it + 1] && this[it + 1] + 1 == this[it + 2] }
+})
+
+//Write a program that reads a number and prints YES if it is positive. Otherwise, the program should print NO.
+fun main6() = readLine()!!.toInt().let { if (it > 0) "YES" else "NO" }.let(::print)
+
+//import kotlin.math.abs
+//There are coordinates of two queens on a chessboard. Find out, whether they take each other.
+// Taking in chess means attacking. The queen moves any number of vacant squares horizontally,
+// vertically, or diagonally.
+
+fun main7() {
+    val (x1, y1) = readLine()!!.split(" ").map { str -> str.toInt() }
+    val (x2, y2) = readLine()!!.split(" ").map { str -> str.toInt() }
+    println(if (x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)) "YES" else "NO")
+}
+
+//Leap year
+
+fun main8() = readLine()!!.toInt()
+    .let { if (it % 4 == 0 && it % 100 != 0 || it % 100 == 0 && it % 400 == 0) "Leap" else "Regular" }.let(::print)
+
+//You have a sequence of integer numbers that ends with 0. Find the largest element of this sequence.
+fun main9() = println(generateSequence { readLine()!!.toInt() }.takeWhile { it != 0 }.maxOrNull())
+
+//Write a program that reads a sequence of numbers of undefined size and prints the largest number
+// and the position of its first occurrence. The position starts from 1.
+fun main10() {
+    val numbers: MutableList<Int> = mutableListOf()
+    var input: Int? = Int.MIN_VALUE
+    while (input != null) {
+        numbers.add(input)
+        input = readLine()?.toIntOrNull()
+    }
+    val max = numbers.maxOrNull()
+    println("$max ${numbers.indexOfFirst { it == max }}")
+}
+
+//Collatz conjecture
+//You have a natural number n. Generate a sequence of integers, described in the Collatz conjecture:
+//If n is an even number, divide it in half, if it is odd, multiply it by 3 and add 1.
+// Repeat this operation until you get 1 as a result.
+
+fun main11() = println(mutableListOf(readLine()!!.toInt()).run {
+    while (last() != 1) this.add(
+        if (last() % 2 == 0) {
+            last() / 2
+        } else last() * 3 + 1
+    )
+    this.joinToString(" ")
 })
